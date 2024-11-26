b61d3dc2d42e1331cd46ef707aadcbb299ed22ce
Raphaël Ribes <raphael.ribes@gmail.com>
2024-11-26 22:20:37 +0100

    Summary updated

--- SAM_specs/1.6_05-02-2020/summarize.py
+++ SAM_specs/1.6_05-02-2020/summarize.py
@@ -1,112 +1,48 @@
-import subprocess, os, shutil, sys
-from matplotlib import pyplot as plt
+import subprocess, os, shutil
 
-def makeLatex(fileName, results, path):
-    latex_content = r"""% Preamble
-\documentclass[11pt]{{article}}
+def make_chromosome(file_name, results, path):
+    latex_content = open(f"{os.path.dirname(os.path.abspath(__file__))}/chromosome.tex", "r").read()
+    latex_content = latex_content.replace("\r\n", "\n")
 
-% Packages
-\usepackage{{amsmath}}
-\usepackage{{fancyhdr}}
-\usepackage{{verbatim}}
+    data_dict = {
+        "M": results["M"],
+        "I": results["I"],
+        "D": results["D"],
+        "S": results["S"],
+        "H": results["H"],
+        "N": results["N"],
+        "P": results["P"],
+        "X": results["X"],
+        "E": results["="],
+        "read_name": file_name,
+        "s_partially_mapped": results["s_partially_mapped"],
+        "p_partially_mapped": results["p_partially_mapped"],
+        "s_unmapped": results["s_unmapped"],
+        "p_unmapped": results["p_unmapped"],
+        "s_mapped": results["s_mapped"],
+        "p_mapped": results["p_mapped"],
+        "s_total": results["s_mapped"] + results["s_partially_mapped"] + results["s_unmapped"],
+        "p_total": results["p_mapped"] + results["p_partially_mapped"] + results["p_unmapped"]
+    }
 
-\begin{{document}}
+    latex_content = latex_content.format(**data_dict)
 
-\title{{Report for {read_name}}}
-\date{{}}
-\maketitle
-
-% stop centering
-\raggedright
-\section{{Read Mapping Summary}}
-
-\noindent
-\begin{{minipage}}[t]{{0.45\textwidth}}
-\centering
-\textbf{{Single Read Summary}} \\
-\begin{{tabular}}{{|l|c|}}
-\hline
-\textbf{{Mapping Type}} & \textbf{{Count}} \\
-\hline
-Mapped & {s_mapped} \\
-Partially Mapped & {s_partially_mapped} \\
-Unmapped & {s_unmapped} \\
-\hline
-Total & {s_total} \\
-\hline
-\end{{tabular}}
-\end{{minipage}}%
-\hfill
-\begin{{minipage}}[t]{{0.45\textwidth}}
-\centering
-\textbf{{Pair Read Summary}} \\
-\begin{{tabular}}{{|l|c|}}
-\hline
-\textbf{{Mapping Type}} & \textbf{{Count}} \\
-\hline
-Mapped & {p_mapped} \\
-Partially Mapped & {p_partially_mapped} \\
-Unmapped & {p_unmapped} \\
-\hline
-Total & {p_total} \\
-\hline
-\end{{tabular}}
-\end{{minipage}}
-
-\section{{Global cigar mutation observed }}
-\centering
-This section only include mapped and partially mapped CIGAR values since unmapped reads do not have one.
-\begin{{table}}[h]
-\centering
-\begin{{tabular}}{{|l|c|}}
-\hline
-\textbf{{Mutation Type}} & \textbf{{Count}} \\
-\hline
-Alignment match & {M} \\
-Insertion to the reference & {I} \\
-Deletion from the reference & {D} \\
-Soft clipping & {S} \\
-Hard clipping & {H} \\
-Skipped region & {N} \\
-Padding & {P} \\
-Sequence match & {X} \\
-Sequence mismatch & {E} \\
-\hline
-\end{{tabular}}
-
-% Annotation under the table
-\scriptsize
-\textcolor{{Note: if there is 100\% of a certain mutation type but $<$0.01\% of any other mutation, the real value of 100\% is less than 100\% due to rounding.}} \\
-\end{{table}}
-
-\thispagestyle{{empty}} % Suppress page number on the title page
-
-
-\end{{document}}""".format(M=results["M"], I=results["I"], D=results["D"], S=results["S"], H=results["H"],
-                           N=results["N"], P=results["P"], X=results["X"], E=results["="],read_name=fileName,
-                           s_partially_mapped=results["s_partially_mapped"],
-                           p_partially_mapped=results["p_partially_mapped"],
-                           s_unmapped=results["s_unmapped"],
-                           p_unmapped=results["p_unmapped"],
-                           s_mapped=results["s_mapped"],
-                           p_mapped=results["p_mapped"],
-                           s_total=results["s_mapped"] + results["s_partially_mapped"] + results["s_unmapped"],
-                           p_total=results["p_mapped"] + results["p_partially_mapped"] + results["p_unmapped"])
-
-    with open(f"{path}/{fileName}.tex", "w") as file:
+    print(os.path.join(path, f"{file_name}.tex"))
+    with open(os.path.join(path, f"{file_name}.tex"), "w") as file:
         file.write(latex_content)
 
     # Compile the .tex file to a .pdf file
+
     with open(os.devnull, 'w') as devnull:
-        subprocess.run(["latexmk", "-silent", f"--output-directory={path}/temp", "-pdf", f"{path}/{fileName}.tex"],
+        subprocess.run(["latexmk", "-silent", f"--output-directory={path}/temp", "-pdf", os.path.join(path, f"{file_name}.tex")],
                        stdout=devnull, stderr=devnull)
 
     # Move the .pdf file to the main directory
-    subprocess.run(["mv", f"{path}/temp/{fileName}.pdf", f"{path}/{fileName}.pdf"])
+    shutil.move(os.path.join(path, 'temp', f"{file_name}.pdf"), os.path.join(path, f"{file_name}.pdf"))
 
 
 def summarize(fileName, results, path, verbose=False, ask_to_open=False):
-    makeLatex(fileName, results, path)
+    make_chromosome(fileName, results, path)
     # Remove the temp folder
     os.remove(f"{path}/{fileName}.tex")
     shutil.rmtree(f"{path}/temp")
