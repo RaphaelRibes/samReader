fb700fa4d9d9b691ec571373c7ddeea748baf1b6
Raphaël Ribes <raphael.ribes@gmail.com>
2024-12-16 00:53:21 +0100

    No interpolation, better x-axis for limited dataset

--- plotit.py
+++ plotit.py
@@ -2,26 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os, shutil
 
-def interpolate(arr: np.array):
-    """
-    Interpolate missing values (empty array) in an array.
-    If the array is already complete, return the original array.
-
-    Args:
-        arr (np.array): Input array with potential NaN values.
-
-    Returns:
-        tuple: A tuple containing:
-            - x (np.array): Array of indices.
-            - interpolated_arr (np.array): Array with NaN values interpolated.
-    """
-    x = np.arange(len(arr))
-    non_nan_indices = np.where(~np.isnan(arr))[0]
-    if len(non_nan_indices) == len(arr):
-        return x, arr
-
-    return x, np.interp(x, non_nan_indices, np.array(arr)[non_nan_indices])
-
 def plot_depth_mapq(depth: np.array,
                     mapq:list[(int, int)],
                     bins=100,
@@ -38,7 +18,21 @@ def plot_depth_mapq(depth: np.array,
         :param depth_median: A boolean indicating whether to use the median of the depth.
         :param bins: The number of bins to divide the data into.
     """
-    ls_depth = np.linspace(0, len(depth), bins + 1, dtype=np.int64)  # Divide the depth into bins
+
+    # Remove the zeros at the beginning and the end of the arrays
+    depth_start_index = np.argmax(depth != 0)  # First non-zero index
+    depth_end_index = len(depth) - np.argmax(depth[::-1] != 0)  # Last non-zero index
+    depth = depth[depth_start_index:depth_end_index]  # Remove the zeros at the beginning and the end
+
+    mapq_start_index = np.argmax(mapq != 0)  # First non-zero index
+    mapq_end_index = len(mapq) - np.argmax(mapq[::-1] != 0)  # Last non-zero index
+    mapq = mapq[mapq_start_index:mapq_end_index]  # Remove the zeros at the beginning and the end
+
+    min_index = min(depth_start_index, mapq_start_index)
+    max_index = max(depth_end_index, mapq_end_index)
+
+    # Divide the depth into bins
+    ls_depth = np.linspace(0, len(depth), bins + 1, dtype=np.int64)
     ls_mapq = np.linspace(0, len(mapq), bins + 1, dtype=np.int64)
 
     classed_depth = [depth[ls_depth[i]:ls_depth[i + 1]] for i in range(bins)]  # Divide the depth into classes
@@ -46,22 +40,22 @@ def plot_depth_mapq(depth: np.array,
 
     # I know this is not pretty, but at least it's clear what's happening
     if depth_median:
-        x_sampled_depth, interpolated_sampled_depth = interpolate(np.array([np.median(cd) if len(cd) > 0 else np.nan for cd in classed_depth]))
-        _, depth_lower_limit = interpolate(np.array([np.percentile(cd, 25) if len(cd) > 0 else np.nan  for cd in classed_depth]))
-        _, depth_upper_limit = interpolate(np.array([np.percentile(cd, 75) if len(cd) > 0 else np.nan  for cd in classed_depth]))
+        sampled_depth = np.array([np.median(cd) if len(cd) > 0 else np.nan for cd in classed_depth])
+        depth_lower_limit = np.array([np.percentile(cd, 25) if len(cd) > 0 else np.nan  for cd in classed_depth])
+        depth_upper_limit = np.array([np.percentile(cd, 75) if len(cd) > 0 else np.nan  for cd in classed_depth])
     else:
-        x_sampled_depth, interpolated_sampled_depth = interpolate(np.array([np.mean(cd) if len(cd) > 0 else np.nan for cd in classed_depth]))
-        depth_lower_limit = np.array([interpolated_sampled_depth[n]-np.std(cd) if len(cd) > 0 else np.nan for n, cd in enumerate(classed_depth)])
-        depth_upper_limit = np.array([interpolated_sampled_depth[n]+np.std(cd) if len(cd) > 0 else np.nan for n, cd in enumerate(classed_depth)])
+        sampled_depth = np.array([np.mean(cd) if len(cd) > 0 else np.nan for cd in classed_depth])
+        depth_lower_limit = np.array([sampled_depth[n]-np.std(cd) if len(cd) > 0 else np.nan for n, cd in enumerate(classed_depth)])
+        depth_upper_limit = np.array([sampled_depth[n]+np.std(cd) if len(cd) > 0 else np.nan for n, cd in enumerate(classed_depth)])
 
     if mapq_median:
-        x_sampled_mapq, interpolated_sampled_mapq = interpolate([np.median(cm) if len(cm) > 0 else np.nan for cm in classed_mapq])
-        _, mapq_lower_limit = interpolate(np.array([np.percentile(cm, 25) if len(cm) > 0 else np.nan for cm in classed_mapq]))
-        _, mapq_upper_limit = interpolate(np.array([np.percentile(cm, 75) if len(cm) > 0 else np.nan for cm in classed_mapq]))
+        sampled_mapq = np.array([np.median(cm) if len(cm) > 0 else np.nan for cm in classed_mapq])
+        mapq_lower_limit = np.array([np.percentile(cm, 25) if len(cm) > 0 else np.nan for cm in classed_mapq])
+        mapq_upper_limit = np.array([np.percentile(cm, 75) if len(cm) > 0 else np.nan for cm in classed_mapq])
     else:
-        x_sampled_mapq, interpolated_sampled_mapq = interpolate([np.mean(cm) if len(cm) > 0 else np.nan for cm in classed_mapq])
-        mapq_lower_limit = np.array([interpolated_sampled_mapq[n]-np.std(cm) if len(cm) > 0 else np.nan for n, cm in enumerate(classed_mapq)])
-        mapq_upper_limit = np.array([interpolated_sampled_mapq[n]+np.std(cm) if len(cm) > 0 else np.nan for n, cm in enumerate(classed_mapq)])
+        sampled_mapq = np.array([np.mean(cm) if len(cm) > 0 else np.nan for cm in classed_mapq])
+        mapq_lower_limit = np.array([sampled_mapq[n]-np.std(cm) if len(cm) > 0 else np.nan for n, cm in enumerate(classed_mapq)])
+        mapq_upper_limit = np.array([sampled_mapq[n]+np.std(cm) if len(cm) > 0 else np.nan for n, cm in enumerate(classed_mapq)])
     # The interpolation doesn't happen all the time, only when some values are missing
     # It's the limitation of this graph, it cannot be precise base-per-base without being confusing and so it has to be interpolated
 
@@ -70,8 +64,8 @@ def plot_depth_mapq(depth: np.array,
     ax2 = ax1.twinx()  # Share the same x-axis
 
     # Plot the depth
-    ax1.plot(x_sampled_depth, interpolated_sampled_depth, label='Mean depth', color='tab:blue')
-    ax1.fill_between(x_sampled_depth, depth_lower_limit, depth_upper_limit, alpha=0.3, label='Interquartile range', color='tab:blue')
+    ax1.plot(sampled_depth, label='Mean depth', color='tab:blue')
+    ax1.fill_between(np.arange(0, len(classed_depth)), depth_lower_limit, depth_upper_limit, alpha=0.3, label='Interquartile range', color='tab:blue')
 
     # Set the labels and limits
     ax1.set_xlim(0, bins-1)
@@ -82,8 +76,8 @@ def plot_depth_mapq(depth: np.array,
     ax1.tick_params(colors='tab:blue', axis='y')
 
     # Plot the mapq
-    ax2.plot(x_sampled_mapq, interpolated_sampled_mapq, color='tab:orange', label='MAPQ')
-    ax2.fill_between(x_sampled_mapq, mapq_lower_limit, mapq_upper_limit, alpha=0.3, color='tab:orange')
+    ax2.plot(sampled_mapq, color='tab:orange', label='MAPQ')
+    ax2.fill_between(np.arange(0, len(classed_mapq)), mapq_lower_limit, mapq_upper_limit, alpha=0.3, color='tab:orange')
     ax2.set_ylabel('MAPQ', color='tab:orange')
     ax2.spines['right'].set_color('tab:orange')
 
@@ -94,7 +88,7 @@ def plot_depth_mapq(depth: np.array,
 
     # Adjust x-ticks to reflect original positions
     ticks = np.linspace(0, len(classed_depth) - 1, n_ticks, dtype=int)
-    tick_labels = np.linspace(0, len(depth) - 1, n_ticks, dtype=int)  # Match to original positions
+    tick_labels = np.linspace(min_index, max_index, n_ticks, dtype=int)  # Match to original positions
 
     ax1.set_xticks(ticks)
     ax1.set_xticklabels([format_size(tick_label) for tick_label in tick_labels], rotation=45)
